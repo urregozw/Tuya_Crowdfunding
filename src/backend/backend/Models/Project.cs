f447c2379b25578888e7ec7f91d96b26c6388ef7
Juan Pablo Gómez <jpgomezxt@gmail.com>
2022-03-22 10:26:38 -0500

    Se crea el modelo del Project

---
+++ src/backend/backend/Models/Project.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace backend.Models
+{
+    public class Project
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("name")]
+        public string Name { get; set; }
+
+        [BsonElement("is_complete")]
+        public bool IsComplete { get; set; }
+
+        [BsonElement("title")]
+        public string Title { get; set; }
+
+        [BsonElement("description")]
+        public string Description { get; set; }
+
+        [BsonElement("project_days")]
+        public int ProjectDays { get; set; }
+
+        [BsonElement("fund_goal")]
+        public float FundGoal { get; set; }
+
+        [BsonElement("backers")]
+        public int Backers { get; set; }
+
+        [BsonElement("funds_collected")]
+        public float FundsCollected { get; set; }
+
+        [BsonElement("entrepreneur")]
+        public virtual Entrepreneur Entrepreneur { get; set; }
+    }
+}
