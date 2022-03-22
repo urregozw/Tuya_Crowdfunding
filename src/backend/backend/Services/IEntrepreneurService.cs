39506965d2eea696224767d4260af0325eda1e3c
Juan Pablo Gómez <jpgomezxt@gmail.com>
2022-03-22 10:27:29 -0500

    Se crea e implementa la interfaz del servicio para las operaciones CRUD sobre el modelo Entrepreneur

---
+++ src/backend/backend/Services/IEntrepreneurService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using backend.Models;
+
+namespace backend.Services
+{
+    public interface IEntrepreneurService
+    {
+        List<Entrepreneur> Get();
+        Entrepreneur Get(string id);
+        Entrepreneur Create(Entrepreneur entrepreneur);
+        void Update(string id, Entrepreneur entrepreneur);
+        void Remove(string id);
+    }
+}
