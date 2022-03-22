b079b5110c00499a1cf019c64e71d9de57650da0
Juan Pablo Gómez <jpgomezxt@gmail.com>
2022-03-22 10:28:04 -0500

    Se crea e implementa la interfaz del servicio de las operaciones CRUD del modelo Project

---
+++ src/backend/backend/Services/ProjectService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using MongoDB.Driver;
+using backend.Models;
+
+namespace backend.Services
+{
+    public class ProjectService : IProjectService
+    {
+        private readonly IMongoCollection<Project> _projects;
+
+        public ProjectService(IDatabaseSettings settings, IMongoClient mongoClient)
+        {
+            var database = mongoClient.GetDatabase(settings.DatabaseName);
+            _projects = database.GetCollection<Project>(settings.ProjectCollectionName);
+        }
+
+        public Project Create(Project project)
+        {
+            _projects.InsertOne(project);
+            return project;
+        }
+
+        public List<Project> Get()
+        {
+            return _projects.Find(project => true).ToList();
+        }
+
+        public Project Get(string id)
+        {
+            return _projects.Find(project => project.Id == id).FirstOrDefault();
+        }
+
+        public void Remove(string id)
+        {
+            _projects.DeleteOne(project => project.Id == id);
+        }
+
+        public void Update(string id, Project project)
+        {
+            _projects.ReplaceOne(project => project.Id == id, project);
+        }
+    }
+}
